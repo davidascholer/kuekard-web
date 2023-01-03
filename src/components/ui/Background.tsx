b69efef3d01ceec902b9876db4b4e21fa0f3d527
David Scholer <davidascholer@gmail.com>
2023-01-02 23:29:33 -0800

    Update Background.tsx

--- src/components/ui/Background.tsx
+++ src/components/ui/Background.tsx
@@ -11,7 +11,7 @@ type Props = {
 const Background: NextPage<Props> = ({children}:Props) => {
   
   return (
-    <div className={styles.body_container}>
+    <div className={styles.body_container} style={{display: "flex",justifyContent: "center", alignItems: "center"}}>
         <HeadComponent/>
         {children}
     </div>
