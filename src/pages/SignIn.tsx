23a7edfdc44a78b14df5c01f586e8bbb52ed882c
David Scholer <davidascholer@gmail.com>
2023-01-02 23:33:42 -0800

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -9,6 +9,7 @@ const SignIn: NextPage = () => {
   return (
     <Background>
           <img src={constructionImage.src}/>
+      <p>Kuecards is going through some major changes. It will be up again by 2-1-23. Please check back!</p>
     </Background>
   );
 };
