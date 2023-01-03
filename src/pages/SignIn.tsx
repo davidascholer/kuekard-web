d96a2e50a28a471a7b430a902fcb907b02892301
David Scholer <davidascholer@gmail.com>
2023-01-02 23:21:50 -0800

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,29 +1,14 @@
 import type { NextPage } from "next";
 import { useState, useRef } from "react";
-import MediaButtonList from "../features/authentication/components/MediaLoginButtonList";
-import FormBody from "../components/form/FormBody";
 import Background from "../components/ui/Background";
-// style
-import logoStyles from "../assets/styles/logo.module.scss";
-// images
-import leftImage from "../assets/images/logo/svg/kuekard_left_128_64.svg";
-import rightImage from "../assets/images/logo/svg/kuekard_right_128_64.svg";
-import rightAltImage from "../assets/images/logo/svg/kuekard_right_alt_128_64.svg";
+import constructionImage from "../assets/images/construction.jpg";
 
 const SignIn: NextPage = () => {
-  const [cardClasses, setCardClasses] = useState<string>("");
-  const rightCard = useRef<HTMLImageElement>(null);
 
-  const flipCard = () => {
-    setCardClasses(`${logoStyles.flip90}`);
-    setTimeout(() => {
-      rightCard.current!.src = rightAltImage.src;
-      setCardClasses(`${logoStyles.flip180}`);
-    }, 500);
-  };
   
   return (
     <Background>
+          <img src={constructionImage.src}/>
     </Background>
   );
 };
